c28d01a2ad86d7f81fb0c417dff34db7621f79d0
krystoliz <109867611+krystoliz@users.noreply.github.com>
2024-10-11 12:08:14 +0700

    <added: more code>

--- main.py
+++ main.py
@@ -7,4 +7,13 @@ from langchain_pinecone import PineconeVectorStore
 
 load_dotenv()
 
-loader = PyPDFLoader()
+loader = PyPDFDirectoryLoader("D:\COOLYEAH\semester 5\pemrosesan bahasa alami\Chatbot-RAG-LLM\health_knowledge_base.txt")
+document =loader.load()
+
+text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+texts = text_splitter.split_documents(document)
+print(f"created {len(texts)} chunks ")
+
+embeddings = OpenAIEmbeddings(openai_api_type=os.environ.get("OPENAI_API_KEY"))
+PineconeVectorStore.from_documents(texts, embeddings, index_name=os.environ.get("INDEX_NAME"))
+
